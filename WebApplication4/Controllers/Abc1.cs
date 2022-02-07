1334b584a8d8d53714c72afcf86ddb7265c8875a
TABKABHAY <abt10112000@gmail.com>
2022-02-07 09:28:28 +0530

    Add project files.

---
+++ WebApplication4/Controllers/Abc1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Controllers
+{
+    public class Abc1 : Controller
+    {
+        private readonly ContactU contactU;
+        public Abc1(ContactU ContactU)
+        {
+            contactU = ContactU;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
